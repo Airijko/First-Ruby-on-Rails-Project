b1eab113285ae4ce656ebaf6d0590de5fecf207d
Airijko <airijkoblade@gmail.com>
2023-10-30 12:24:05 -0500

    added many to many relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     belongs_to :gender, foreign_key: 'genders_id'
     belongs_to :country
     has_many :posts
+    has_many :user_titles
+    has_many :titles, through: :user_titles
 end
